1ba444aceb8119fa42e9ba4d722a7922b1cc282d
thejohncotton <john@1080people.com>
2017-08-27 14:48:29 -0400

    add an admin page for UD elements of CRUD

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
-
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
     def home
         @posts = Post.all
     end
+
+    def admin
+        @posts = Post.all
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get  "/admin123456" => 'static_pages#admin'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
