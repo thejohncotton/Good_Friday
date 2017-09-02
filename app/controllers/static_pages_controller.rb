2c0d3345d1843c659f23af99b884b0955df3049d
thejohncotton <john@1080people.com>
2017-09-02 17:12:25 -0400

    update order in viewsControllers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
     def home
-        @posts = Post.all
+        @posts = Post.all.order(created_at: :desc)
     end
 
     def admin
-        @posts = Post.all
+        @posts = Post.all.all.order(created_at: :desc)
     end
 
 
