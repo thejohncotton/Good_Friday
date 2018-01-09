686c59f1061332d01fa4343023dcaa0406c67e50
thejohncotton <john@1080people.com>
2018-01-09 12:27:57 -0500

    add json api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  "/admin123456" => 'static_pages#admin'
   resources :posts
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
